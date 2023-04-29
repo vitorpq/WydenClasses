e197fe0ecc72e4fe0e743acd67a337f1fbe30ee3
Vitor E Andrade <vitor@vitor.one>
2023-04-29 15:35:03 -0300

    Finalização do programa pwd.py. Falta implementar encryption.

--- pwd.py
+++ pwd.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python3
 # -*- utf-8 -*-
 """
-	Usage: pwd.py add username
-	
+    Usage: pwd.py add username
+
 """
 
 import sys
-
-from pwdlibs import AddUser, UpdateUser
-
+from pwdlibs import lerArquivo, checkUser, AddUser
 
 if len(sys.argv) == 1:
-	
-	comando = input("Entre o comando desejado - add ou update: ")
-	
-	while comando != ("add" or "update"):
-		comando = input("Comando inválido. Entre uma opção válida - add ou update: ")
+    arquivo = "password.txt"
+    dados = lerArquivo(arquivo)
+    comando = input("Entre o comando desejado - add ou update: ")
+    while not (comando == "add" or comando == "update"):
+        comando = input("Comando inválido. Entre uma opção válida - add ou update: ")
 
-	if comando == "add":
-		
-		AddUser()
-		
-	elif comando == "update":
-		UpdateUser()
-		
+    # --- Add user ---
+    if comando == "add":
+        """
+            Verifica se o usuário existe, se exixtir, pede outro nome
+            Se não existir, pede a senha 2x
+        """
+        # pede nome usuario
+        usuario = input("Qual o nome de usuário a ser adicionado? ")
+        # checa se o usuário já existe
+        while checkUser(dados, usuario.lower()):
+            usuario = input("Usuário já existe!\nFavor utilizar outro: ")
+        if not checkUser(dados, usuario):
+            AddUser(arquivo, usuario)
+
+    # --- Update User ---
+    elif comando == "update":
+        # pede nome do usuário e valida se ele existe
+        usuario = input("Nome de usuário: ")
+        AddUser(arquivo, usuario)
--- teste.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-# -*- utf-8 -*-
-
-from pwdlibs import lerArquivo, UpdateUser, checkUser
-
-print(lerArquivo("password.txt"))
